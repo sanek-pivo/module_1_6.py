f9121a28de93cf5d4f3c1e30eef9233841f15e77
sanek-pivo <kapelka_555@mail.ru>
2024-09-05 19:16:50 +0700

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Sasha': 1980, 'Miha': 1981}
+print(my_dict)
+print(my_dict.get('Sasha'))
+my_dict['Dad'] = 1955
+print(my_dict)
+my_dict.update({'son-1': 2008,'son-2': 2016})
+print(my_dict)
+del my_dict['Dad']
+print(my_dict)
+my_set = {4, 3, 2, 1, 4, 3, 5, "str"}
+print(my_set)
+my_set = {4, 3, 2, 1, 4, 3, 5, 6, 7, "str"}
+print(my_set)
+print(my_set.remove(1))
+print(my_set)
